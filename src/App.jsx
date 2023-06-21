447578b6ec120f19f1ad2922b9f09e7c082f5b2d
Arjun024 <mallikarjuna.gunisetti@gmail.com>
2023-06-21 11:56:47 +0530

    feat: create todos

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import CreateTodo from './components/CreateTodo'
+import { Container } from '@mui/material'
+
+function App() {
+  return (
+    <Container maxWidth="md">
+        App
+        <CreateTodo />
+    </Container>
+  )
+} 
+
+export default App
